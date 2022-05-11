954071efd527eef46f3d03e9780626c14ece39d3
Your Namegit config --global user.name Your <you@example.com>
2022-05-11 18:25:47 -0400

    delete route

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -37,11 +37,18 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex
     })
   } else {
     res.render('404')
   }
+})
+
+// DELETE
+breads.delete('/:arrayIndex', (req, res) =>{
+  Bread.splice(req.params.arrayIndex, 1);
+  res.status(303).redirect('/breads');
 })
 
 
