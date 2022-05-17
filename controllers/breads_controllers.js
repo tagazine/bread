e7239f40a431a2fff2b50c25d196b29b532c71f8
Your Namegit config --global user.name Your <you@example.com>
2022-05-16 20:22:09 -0400

    Manipulate that data, baby

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -4,71 +4,69 @@ const Bread = require("../models/bread.js");
 
 // Index
 breads.get("/", (req, res) => {
-  res.render('Index', {
-    breads: Bread
+  Bread.find().then((foundBreads) => {
+    res.render("index", {
+      breads: foundBreads,
+      title: "Index Page",
+    });
   });
 });
 
 module.exports = breads;
 
 // CREATE
-breads.post('/', (req, res) => {
+breads.post("/", (req, res) => {
   if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    req.body.image = undefined;
   }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
   } else {
-    req.body.hasGluten = false
+    req.body.hasGluten = false;
   }
-  Bread.push(req.body)
-  res.redirect('/breads')
-})
-
+  Bread.create(req.body);
+  res.redirect("/breads");
+});
 
 // NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
-})
-
+breads.get("/new", (req, res) => {
+  res.render("new");
+});
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex
+breads.get("/:id", (req, res) => {
+  Bread.findById(req.params.id)
+    .then((foundBread) => {
+      res.render("show", {
+        bread: foundBread,
+      });
     })
-  } else {
-    res.render('404')
-  }
-})
+    .catch(err => {
+      res.render("404");
+    });
+});
 
 // DELETE
-breads.delete('/:arrayIndex', (req, res) =>{
+breads.delete("/:arrayIndex", (req, res) => {
   Bread.splice(req.params.arrayIndex, 1);
-  res.status(303).redirect('/breads');
-})
+  res.status(303).redirect("/breads");
+});
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
+breads.put("/:arrayIndex", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
   } else {
-    req.body.hasGluten = false
+    req.body.hasGluten = false;
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
-})
+  Bread[req.params.arrayIndex] = req.body;
+  res.redirect(`/breads/${req.params.arrayIndex}`);
+});
 
 // EDIT
-breads.get('/:arrayIndex/edit', (req, res) => {
-  res.render('edit', {
+breads.get("/:arrayIndex/edit", (req, res) => {
+  res.render("edit", {
     bread: Bread[req.params.arrayIndex],
-    index: req.params.arrayIndex
-  })
-})
-
-
-
-
+    index: req.params.arrayIndex,
+  });
+});
